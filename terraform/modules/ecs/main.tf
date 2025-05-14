d513021fedbf4df16ee504d5c40bb42a6593c0a0
mnaviapi <156114896+mnaviapi@users.noreply.github.com>
2025-05-14 19:30:30 -0400

    Update ecs main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -11,18 +11,19 @@ resource "aws_ecs_task_definition" "this" {
   family                   = "csgtest-${var.environment}-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = var.cpu
-  memory                   = var.memory
-  execution_role_arn       = var.execution_role_arn
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = var.task_execution_role
 
   container_definitions = jsonencode([
     {
-      name      = "app"
-      image     = var.container_image
+      name      = "nginx"
+      image     = var.image_url
       essential = true
       portMappings = [
         {
           containerPort = var.container_port
+          hostPort      = var.container_port
         }
       ]
     }
@@ -33,13 +34,13 @@ resource "aws_ecs_service" "this" {
   name            = "csgtest-${var.environment}-service"
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.this.arn
-  launch_type     = "FARGATE"
   desired_count   = 1
+  launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = var.subnets
+    subnets          = var.subnet_ids
     assign_public_ip = true
-    security_groups = var.security_groups
+    security_groups  = [] # Puedes pasar uno si quieres
   }
 
   depends_on = [aws_ecs_task_definition.this]
