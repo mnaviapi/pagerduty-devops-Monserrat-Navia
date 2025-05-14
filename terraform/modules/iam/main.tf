ef2933083524bec6f95fd6c0d91652dd23dc31cb
mnaviapi <156114896+mnaviapi@users.noreply.github.com>
2025-05-14 19:13:33 -0400

    IAM main.tf

---
+++ terraform/modules/iam/main.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "ecs_execution" {
+  name = "ecsTaskExecutionRole-${var.environment}"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+
+  tags = {
+    Name        = "csgtest-${var.environment}-ecs-role"
+    environment = var.environment
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
