660ef82539759c8b9c4af6dbe08b836f53b5b47a
mnaviapi <156114896+mnaviapi@users.noreply.github.com>
2025-05-19 03:33:17 -0400

    Update outputs.tf

--- terraform/modules/vpc/outputs.tf
+++ terraform/modules/vpc/outputs.tf
@@ -1,9 +1,7 @@
 output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.main.id
+  value = aws_vpc.this.id
 }
 
 output "public_subnet_1_id" {
-  description = "ID of the first public subnet"
-  value       = aws_subnet.public_1.id
+  value = aws_subnet.public_1.id
 }
