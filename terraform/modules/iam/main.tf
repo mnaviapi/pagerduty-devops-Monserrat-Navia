8bcd3d78bddf0eede2c582f7947e20d769075de3
mnaviapi <156114896+mnaviapi@users.noreply.github.com>
2025-05-19 03:58:47 -0400

    Update iam main.tf

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -5,11 +5,11 @@ resource "aws_iam_role" "ecs_task_execution_role" {
     Version = "2012-10-17",
     Statement = [
       {
-        Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        }
+        },
+        Action = "sts:AssumeRole"
       }
     ]
   })
