90250e2a4085fd64ce3c642c2ad5df8f15a98a60
mnaviapi <156114896+mnaviapi@users.noreply.github.com>
2025-05-19 03:08:02 -0400

    ecs logs main.tf

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -7,6 +7,11 @@ resource "aws_ecs_cluster" "this" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "alert_builder" {
+  name              = "/ecs/alert-builder"
+  retention_in_days = 7
+}
+
 resource "aws_ecs_task_definition" "this" {
   family                   = "csgtest-${var.environment}-task"
   requires_compatibilities = ["FARGATE"]
@@ -25,7 +30,15 @@ resource "aws_ecs_task_definition" "this" {
           containerPort = var.container_port
           hostPort      = var.container_port
         }
-      ]
+      ],
+      logConfiguration = {
+        logDriver = "awslogs",
+        options = {
+          awslogs-group         = "/ecs/alert-builder",
+          awslogs-region        = "us-east-1",
+          awslogs-stream-prefix = "ecs"
+        }
+      }
     }
   ])
 }
